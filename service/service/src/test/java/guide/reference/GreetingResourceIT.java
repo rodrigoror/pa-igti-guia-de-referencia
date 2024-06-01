834486a445a9b158b70121d3e9a4e05317aad86b
rodrigoror <rodrigoror@gmail.com>
2024-06-01 09:59:43 -0300

    Teoria/Prática - Item 1 - Adequação do Framework Quarkus

---
+++ service/service/src/test/java/guide/reference/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package guide.reference;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
